92f30d8cd4c0ccbab94f81e7d7aa878b3f24ec4e
ali829 <alielyassraoui@gmail.com>
2024-05-23 21:39:56 +0100

    get track visitorapi

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ window.onload = () => {
       notes: "Check this out!",
     };
 
-    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams).then(
+    emailjs.send("service_o02rl8j", "template_hi1blda", templateParams).then(
       (response) => {
         console.log("SUCCESS!", response.status, response.text);
       },
