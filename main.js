671a059444b2b76db9f11d6eca922a76b37edc7a
ali829 <alielyassraoui@gmail.com>
2024-05-23 20:40:35 +0100

    get track visitorapi

--- main.js
+++ main.js
@@ -6,10 +6,33 @@ window.onload = () => {
   const communitiesContainer = document.querySelector(".communities-container");
   if (isMobileDevice()) {
     let filteredDataOnMobile = data.filter(
-        (sm) => (sm.name !== "discord" && sm.name !== "e-mail") 
-      );
-      render(communitiesContainer,filteredDataOnMobile)
+      (sm) => sm.name !== "discord" && sm.name !== "e-mail"
+    );
+    render(communitiesContainer, filteredDataOnMobile);
   } else {
     render(communitiesContainer, data);
   }
+
+  var VisitorAPI = function (t, e, a) {
+    var s = new XMLHttpRequest();
+    (s.onreadystatechange = function () {
+      var t;
+      s.readyState === XMLHttpRequest.DONE &&
+        (200 === (t = JSON.parse(s.responseText)).status
+          ? e(t.data)
+          : a(t.status, t.result));
+    }),
+      s.open("GET", "https://api.visitorapi.com/api/?pid=" + t),
+      s.send(null);
+  };
+
+  VisitorAPI(
+    "sf7NkNqC2XHs7x4I6cTz",
+    function (data) {
+      console.log(data);
+    },
+    function (errorCode, errorMessage) {
+      console.log(errorCode, errorMessage);
+    }
+  );
 };
